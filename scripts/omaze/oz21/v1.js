56947514675eadda8874b3f795881e0790ffc663
Simon Cordova <simoncordova@Simons-MacBook-Pro.local>
2025-07-10 11:05:04 +0100

    Update OZ21

--- scripts/omaze/oz21/v1.js
+++ scripts/omaze/oz21/v1.js
@@ -69,19 +69,22 @@ const svgIcons = {
               </svg>`
 };
 
-// Selectors for the Shopify containers
-const SELECTOR_NAVIGATION_BAR = '#main-nav';
-const SELECTOR_SHOPIFY_CONTAINERS = 'main .shopify-section > div[id*=enter-now-]';
-const SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN = '#enter-now-material-tab-buttons-design';
-const SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN_PRESELECT_PAYG = '#enter-now-material-tab-buttons-design-preselect-payg';
-const SELECTOR_CONTAINER_ENTER_NOW_BIG_TAB_BUTTONS_DESIGN = '#enter-now-big-tab-buttons-design';
-const SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN = '#enter-now-legacy-design';
-const SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN_NO_SUBS = '#enter-now-legacy-design-no-subs';
-const SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN_NO_SUBS_LOWER_PRICE_POINTS = '#enter-now-legacy-design-no-subs-lower-price-points';
+const selectors = {
+  SELECTOR_NAVIGATION_BAR: '#main-nav',
+  SELECTOR_SHOPIFY_CONTAINERS: 'main .shopify-section > div[id*=enter-now-]',
+  SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN: '#enter-now-material-tab-buttons-design',
+  SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN_PRESELECT_PAYG: '#enter-now-material-tab-buttons-design-preselect-payg',
+  SELECTOR_CONTAINER_ENTER_NOW_BIG_TAB_BUTTONS_DESIGN: '#enter-now-big-tab-buttons-design',
+  SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN: '#enter-now-legacy-design',
+  SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN_NO_SUBS: '#enter-now-legacy-design-no-subs',
+  SELECTOR_CONTAINER_ENTER_NOW_LEGACY_DESIGN_NO_SUBS_LOWER_PRICE_POINTS: '#enter-now-legacy-design-no-subs-lower-price-points',
+  SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN_BUTTONS: '#enter-now-material-tab-buttons-design .add-to-cart-button',
+  SELECTOR_ENTER_NOW_LEGAGY_DESIGN_BUTTONS: '#enter-now-legacy-design [data-test*=card-variant] .add-to-cart-button',
+  SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN_BUTTONS: '#enter-now-material-tab-buttons-design .add-to-cart-button',
+  SELECTOR_ENTER_NOW_LEGAGY_DESIGN_BUTTONS: '#enter-now-legacy-design [data-test*=card-variant] .add-to-cart-button',
+}
 
 // Selectors for the buttons
-const SELECTOR_CONTAINER_ENTER_NOW_MATERIAL_TAB_BUTTONS_DESIGN_BUTTONS = '#enter-now-material-tab-buttons-design .add-to-cart-button';
-const SELECTOR_ENTER_NOW_LEGAGY_DESIGN_BUTTONS = '#enter-now-legacy-design [data-test*=card-variant] .add-to-cart-button';
 
 const newTrophyIcon = '<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.802 5.321C7.761 5.359 7.75 5.396 7.75 5.423V10.961C7.75 12.361 8.136 13.561 8.833 14.398C9.516 15.216 10.548 15.75 12 15.75C13.452 15.75 14.484 15.216 15.167 14.398C15.864 13.562 16.25 12.361 16.25 10.961V5.423C16.25 5.396 16.24 5.359 16.198 5.321C16.1427 5.27421 16.0724 5.24899 16 5.25H8C7.92759 5.24899 7.85727 5.27421 7.802 5.321ZM6.25 5.423C6.25 4.443 7.091 3.75 8 3.75H16C16.909 3.75 17.75 4.444 17.75 5.423V5.75H19C19.966 5.75 20.75 6.534 20.75 7.5V9.5C20.75 11.408 19.223 12.921 17.443 13.203C17.2193 13.9905 16.8368 14.7239 16.319 15.358C15.466 16.38 14.259 17.046 12.75 17.21V19.75H15C15.1989 19.75 15.3897 19.829 15.5303 19.9697C15.671 20.1103 15.75 20.3011 15.75 20.5C15.75 20.6989 15.671 20.8897 15.5303 21.0303C15.3897 21.171 15.1989 21.25 15 21.25H9C8.80109 21.25 8.61032 21.171 8.46967 21.0303C8.32902 20.8897 8.25 20.6989 8.25 20.5C8.25 20.3011 8.32902 20.1103 8.46967 19.9697C8.61032 19.829 8.80109 19.75 9 19.75H11.25V17.21C9.741 17.045 8.534 16.38 7.681 15.358C7.16324 14.7239 6.78073 13.9905 6.557 13.203C4.777 12.92 3.25 11.408 3.25 9.5V7.5C3.25 6.534 4.034 5.75 5 5.75H6.25V5.423ZM6.25 7.25H5C4.9337 7.25 4.87011 7.27634 4.82322 7.32322C4.77634 7.37011 4.75 7.4337 4.75 7.5V9.5C4.75 10.41 5.394 11.238 6.272 11.582C6.25754 11.3756 6.25021 11.1689 6.25 10.962V7.25ZM17.728 11.582C18.606 11.238 19.25 10.41 19.25 9.5V7.5C19.25 7.4337 19.2237 7.37011 19.1768 7.32322C19.1299 7.27634 19.0663 7.25 19 7.25H17.75V10.961C17.75 11.1697 17.7427 11.3767 17.728 11.582Z" fill="#626262"/></svg>';
 
@@ -385,10 +388,15 @@ function enableSecondaryNavActiveState() {
 const waitForElements = (elementSelector) => {
   customLog('[waitForElements] Starting to wait for cards...');
 
-  DYO.waitForElementAsync(elementSelector, 1, 100, 150)
-    .then(function (elements) {
-      // Element is ready and exists exactly once
-      console.log('Element found:', elements[0]);
+  // Wait for both the navigation bar and the site footer
+  Promise.all([
+    DYO.waitForElementAsync(elementSelector, 1, 100, 150),
+    DYO.waitForElementAsync('.site-footer', 1, 100, 150)
+  ])
+    .then(function ([navElements, footerElements]) {
+      // Both elements are ready and exist exactly once
+      console.log('Element found:', navElements[0]);
+      console.log('Site footer found:', footerElements[0]);
 
       // Add styles to the document
       addStyles(styles);
@@ -398,10 +406,9 @@ const waitForElements = (elementSelector) => {
       enableSecondaryNavActiveState();
     })
     .catch(function () {
-      // Element not found within 15 seconds
-      console.warn('#main-nav not found within timeout.');
+      // One or both elements not found within 15 seconds
+      console.warn('#main-nav or .site-footer not found within timeout.');
     });
-
 };
 
 async function init() {
@@ -414,7 +421,7 @@ async function init() {
     document.body.classList.add('omaze-oz21-qa-v1');
     customLog('[init] Added class omaze-oz21-qa-v1 to body');
 
-    waitForElements(SELECTOR_NAVIGATION_BAR);
+    waitForElements(selectors.SELECTOR_NAVIGATION_BAR);
 
   } catch (error) {
     console.error(error.message);
